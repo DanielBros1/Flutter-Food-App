92b8096136b7c5fcc71f3443463812667f86857f
DanielBros1 <danielbros41@gmail.com>
2023-11-28 20:58:20 +0100

    #15 Simple refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Food App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
         useMaterial3: true,
